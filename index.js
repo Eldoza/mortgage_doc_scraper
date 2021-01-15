b470b092145572f8fdd91830b3e8302822513392
Chris Mendoza <cmend85@gmail.com>
2021-01-14 23:48:30 -0600

    Mo stuff

--- index.js
+++ index.js
@@ -1,38 +1,65 @@
 const xlsx = require("node-xlsx");
 const axios = require("axios");
 const fs = require("fs");
+const { queryToInsertDocument } = require("./queries/documents");
+
+// PIN 1083852 already in DB 
+// 1-10
+// 11-100
+// 101-500
+
+
+let counter = 500
+const delay = interval => new Promise(resolve => setTimeout(resolve, interval))
 
 const getXLS = () => {
   return axios
     .request({
       responseType: "arraybuffer",
-      url: "http://www.ccrecorder.org/parcels/excel/parcel/1083852/",
+      url: `http://www.ccrecorder.org/parcels/excel/parcel/${counter}/`,
       method: "get",
       headers: {
         "Content-Type": "blob",
       },
     })
-    .then((result) => {
+    .then(async (result) => {
       const workSheetsFromBuffer = xlsx.parse(result.data, { cellDates: true });
 
       const dataFromSheet = workSheetsFromBuffer[0].data;
-      const results = [];
+      const documentResults = [];
 
       for (let i = 1; i < dataFromSheet.length; i++) {
         const columns = dataFromSheet[0];
         const currentElement = dataFromSheet[i];
 
-        const newElement = Object.assign(
+        const newDocument = Object.assign(
           ...columns.map((k, i) => ({ [k]: currentElement[i] }))
         );
 
-        results.push(newElement);
-      }
-      
-      console.log({ results });
+        const formattedDocument = {
+          docDate: newDocument["Doc Date"] || null,
+          docType: newDocument["Doc Type"] || null,
+          docNumber: newDocument["Doc Number"] || null,
+          ...newDocument,
+        };
 
-      // return outputFilename;
+        documentResults.push(formattedDocument);
+        await queryToInsertDocument(formattedDocument);
+      }
+
+      console.log({ documentResults });
     });
 };
 
-getXLS();
+
+const runFetchDocsJob = async () => {
+  while (counter < 500){
+    getXLS();
+    await delay(1000)
+    counter += 1
+    
+    console.log('COUNTER CURRENTLY AT ===>', counter)
+  }
+}
+
+runFetchDocsJob()
---
+++ queries/documents.js
@@ -0,0 +1,30 @@
+const { db, pgp } = require("../dbConnection");
+
+exports.queryToInsertDocument = (document) => {
+  return db.none(
+    `INSERT 
+      INTO
+      documents (
+        doc_date,
+        doc_type,
+        doc_number,
+        pin,
+        address,
+        city,
+        zipcode,
+        parties_involved
+      )
+      VALUES (
+        $/docDate/,
+        $/docType/,
+        $/docNumber/,
+        $/PIN/,
+        $/Address/,
+        $/City/,
+        $/Zipcode/,
+        $/Parties/
+      )
+    `,
+    { ...document }
+  );
+};
